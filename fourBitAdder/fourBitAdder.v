36deb93b6276e6182c76863cf6583c1dabaa036d
bhendi-boi <bhendiisgreen@gmail.com>
2023-05-06 11:48:54 +0530

    updated fourBitAdder - instead of including fullAdder module in design file we import it from existing design

--- fourBitAdder/fourBitAdder.v
+++ fourBitAdder/fourBitAdder.v
@@ -1,15 +1,4 @@
-module fullAdder (
-    output sum,carryOut, input a,b,carryIn
-);
-    wire w0,w1,w2,w3;
-    xor x0(w0,a,b);
-    xor x1(sum,carryIn,w0);
-    and a0(w1,a,b);
-    and a1(w2,b,carryIn);
-    and a2(w3,carryIn,a);
-    or o0(carryOut,w1,w2,w3);
-endmodule
-
+`include "../full-adder/fullAdder.v"
 module 
     fourBitAdder (
     output [3:0] sum,output carryOut,input[3:0] a,b,input carryIn
